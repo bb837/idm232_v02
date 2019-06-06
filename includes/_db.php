157d603ad498261d8e5b2f36215cf37c0705ce8c
bb837 <43794585+bb837@users.noreply.github.com>
2019-06-06 17:05:35 -0400

    Update _db.php

--- includes/_db.php
+++ includes/_db.php
@@ -1,8 +1,21 @@
 <?php
+
+$host = $_SERVER['HTTP_HOST'];
+if ($host == 'localhost') {
+
 define('DB_SERVER', 'localhost');       
 define('DB_USER', 'root');
 define('DB_PASS', '');
 define('DB_NAME', 'idm-232-lets-cook');
+
+}
+else {
+  // Remote database credentials
+  define('DB_SERVER', 'www.briannabuissereth.com');
+  define('DB_USER', 'briannp5_covibui');
+  define('DB_PASS', 'Alexander03');
+  define('DB_NAME', 'briannp5_idm232');
+}
 
 $connection = mysqli_connect(DB_SERVER, DB_USER, DB_PASS, DB_NAME);
 
