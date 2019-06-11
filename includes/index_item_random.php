18d0c8f69d00cd790afdfb7babe01bd7cbc7dd40
bb837 <43794585+bb837@users.noreply.github.com>
2019-06-11 02:15:42 -0400

    Update index_item_random.php
    Updated to include multiple iterations

--- includes/index_item_random.php
+++ includes/index_item_random.php
@@ -1,7 +1,7 @@
 <?php
  while($recipe = mysqli_fetch_assoc($result)) {
     if (in_array($recipe["id"], $random_id)) {
-        require_once "includes/index_item.php"
+        include "includes/index_item.php";
     }
  }
  ?>
